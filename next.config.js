f3e579e8ce0381579854410dee44a1a843954ff3
9th-Bacar <932581735@qq.com>
2023-04-14 18:44:46 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // assetPrefix: ".",
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
